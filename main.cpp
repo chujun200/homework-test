c32f071a9035ebc25738d5dca2b4975c0311f45c
chujun <1009494760@qq.com>
2020-02-21 14:57:21 +0800

    creat checkPrimeNumber

--- main.cpp
+++ main.cpp
@@ -4,21 +4,30 @@
 #define MinN 2     //下界
 #define MaxN 100     //上界
 using namespace std;
+
+int checkPrimeNumber(int n) 
+{
+	int m;
+	if (n <= 1)
+		return 0;
+	for (m = 2; m <= n; m++)
+	{
+		if (n%m == 0)
+			break;
+	}
+	if (n == m)
+		return 1;
+	else
+		return 0;
+};
 int main()
 {
-	int n, m;
+	int n;
 	cout << "The Prime number from "<< MinN <<" to "<<MaxN<<endl;
 	for (n = MinN; n <= MaxN; n++)
 	{
-		for (m = 2; m <= n; m++)
-		{
-			if (n%m == 0)
-				break;
-		}
-		if (n == m)
-		{
+		if (checkPrimeNumber(n) == 1)
 			cout <<n <<endl;
-		}
 	}
 	return 0;
 }
